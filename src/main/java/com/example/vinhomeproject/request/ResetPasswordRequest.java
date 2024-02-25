eb3257219e70e317342c1d088c89a6b50bb17c6d
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-02-25 13:49:39 +0700

    send mail, reset password, verify code API

---
+++ src/main/java/com/example/vinhomeproject/request/ResetPasswordRequest.java
@@ -0,0 +1,12 @@
+package com.example.vinhomeproject.request;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordRequest {
+    String newPassword;
+    String email;
+}
