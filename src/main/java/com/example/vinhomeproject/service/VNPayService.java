d93d0cd29a160e5de767c5533a157ec91b72ea78
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-02-26 23:05:49 +0700

    update timezone in VNPay API

--- src/main/java/com/example/vinhomeproject/service/VNPayService.java
+++ src/main/java/com/example/vinhomeproject/service/VNPayService.java
@@ -87,7 +87,7 @@ public class VNPayService {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-
+        cld.add(Calendar.HOUR_OF_DAY, 7);
         cld.add(Calendar.MINUTE, 15);
         String vnp_ExpireDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
