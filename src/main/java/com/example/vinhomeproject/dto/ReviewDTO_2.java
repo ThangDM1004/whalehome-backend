80ba9b9957c470e2ee93c4baeb23be30e20a6f36
hieu15402 <100761394+hieu15402@users.noreply.github.com>
2024-03-07 16:10:49 +0700

    update-get-page-review

--- src/main/java/com/example/vinhomeproject/dto/ReviewDTO_2.java
+++ src/main/java/com/example/vinhomeproject/dto/ReviewDTO_2.java
@@ -1,13 +1,12 @@
 package com.example.vinhomeproject.dto;
 
-import com.example.vinhomeproject.models.Apartment;
-import com.example.vinhomeproject.models.Users;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
+
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @RequiredArgsConstructor
@@ -18,5 +17,6 @@ public class ReviewDTO_2 {
     private String content;
     private double rate;
     private Long apartmentId;
+    private Long usersid;
     private boolean status;
 }
