a4c18c28177e0be38227beceddaba8e245b11685
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-03-12 13:09:16 +0700

    fix response API

---
+++ src/main/java/com/example/vinhomeproject/dto/UserAppointmentDTO.java
@@ -0,0 +1,14 @@
+package com.example.vinhomeproject.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserAppointmentDTO {
+    private String name;
+    private String image_url;
+    private String phone;
+}
