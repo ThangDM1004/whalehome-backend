63ec6c834ac12521e6993bd1aa963caa082893dc
hieu15402 <100761394+hieu15402@users.noreply.github.com>
2024-03-17 16:21:55 +0700

    create payment and update calculate revenueByYear

--- src/main/java/com/example/vinhomeproject/models/Payment.java
+++ src/main/java/com/example/vinhomeproject/models/Payment.java
@@ -5,10 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -20,6 +17,7 @@ import java.util.Locale;
 @AllArgsConstructor
 @Entity
 @Table
+@Builder
 public class Payment extends Base{
 
     private LocalDate payment_time;
