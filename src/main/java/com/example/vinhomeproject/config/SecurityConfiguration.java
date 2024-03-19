09a5f483e9e886d82a17715ac6338df18f2e2351
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-03-19 10:40:44 +0700

    update payment API

--- src/main/java/com/example/vinhomeproject/config/SecurityConfiguration.java
+++ src/main/java/com/example/vinhomeproject/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,12 +23,10 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfiguration {
 
     private static final String[] WHITE_LIST_URL = {
-            "/api/v1/auth/**",
             "/v3/api-docs/**",
             "/v3/api-docs.yaml/**",
             "/swagger-ui/**",
             "/swagger-ui.html",
-            "/api/v1/**",
             "/**"
     };
     @Autowired
@@ -43,8 +42,77 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
-//                                .requestMatchers("/swagger-ui/**").hasAnyRole(ADMIN.name())
-//                                .requestMatchers("/swagger-ui/**").hasAnyRole(USER.name())
+                                .requestMatchers(HttpMethod.GET, "/api/v1/apartmentclasses/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/apartmentclasses/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/apartmentclasses/**").hasAnyRole("ADMIN")
+                                //---------------- apartment ---------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/apartments/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/apartments/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/apartments/**").hasAnyRole("ADMIN")
+                                //---------------- appointment -------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/appointments/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/appointments/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/appointments/**").hasAnyRole("ADMIN")
+                                //---------------- area -------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/areas/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/areas/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/areas/**").hasAnyRole("ADMIN")
+                                //---------------- authentication ---------------------
+                                .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").hasAnyRole( "ADMIN", "USER")
+                                //---------------- bank -------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/bank/**").hasAnyRole( "ADMIN", "USER")
+                                //---------------- building ---------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/buildings/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/buildings/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/buildings/**").hasAnyRole("ADMIN")
+                                //---------------- card -------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/card/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/card/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/card/**").hasAnyRole("ADMIN")
+                                //---------------- contract ---------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/contracts/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/contracts/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/contracts/**").hasAnyRole("ADMIN")
+                                //---------------- contract history -------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/contracthistories/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/contracthistories/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/contracthistories/**").hasAnyRole("ADMIN")
+                                //---------------- issue -------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/issue/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/issue/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/issue/**").hasAnyRole("ADMIN")
+                                //---------------- payment -----------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/payment/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/payment/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/payment/**").hasAnyRole("ADMIN")
+                                //---------------- post ---------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/post/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/post/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/post/**").hasAnyRole("ADMIN")
+                                //---------------- post image ---------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/postimage/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/postimage/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/postimage/**").hasAnyRole("ADMIN")
+                                //---------------- problem ------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/problem/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/problem/**").hasAnyRole("ADMIN","USER")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/problem/**").hasAnyRole("ADMIN","USER")
+                                //---------------- problem image ------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/problem-image/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/problem-image/**").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/problem-image/**").hasAnyRole("ADMIN", "USER")
+                                //---------------- review -------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/review/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/contracts/**").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/contracts/**").hasAnyRole("ADMIN", "USER")
+                                //---------------- user ---------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/user/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/user/**").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/user/**").hasAnyRole("ADMIN", "USER")
+                                //---------------- zone ---------------------------------
+                                .requestMatchers(HttpMethod.GET, "/api/v1/zone/**").hasAnyRole( "ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST,"/api/v1/zone/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/zone/**").hasAnyRole("ADMIN")
                                 .anyRequest()
                                 .authenticated()
                 )
