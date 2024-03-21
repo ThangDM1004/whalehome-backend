a7bfde0f372ef4ea2994dfde0bc633a3280d2866
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-03-21 13:46:11 +0700

    socket webconfig update

--- src/main/java/com/example/vinhomeproject/config/WebSocketConfig.java
+++ src/main/java/com/example/vinhomeproject/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
