f21bb7745c9f21dea09125a6dc85aba6014a426e
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-03-21 13:45:45 +0700

    socket webconfig update

--- src/main/java/com/example/vinhomeproject/config/WebSocketConfig.java
+++ src/main/java/com/example/vinhomeproject/config/WebSocketConfig.java
@@ -7,6 +7,7 @@ import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -17,6 +18,7 @@ import java.util.List;
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
+@CrossOrigin("*")
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
