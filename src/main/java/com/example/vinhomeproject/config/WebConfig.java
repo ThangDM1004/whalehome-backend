293ad7f20c5478f92d25ebe541ab94e42b3373a9
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-03-21 13:41:05 +0700

    socket webconfig update

--- src/main/java/com/example/vinhomeproject/config/WebConfig.java
+++ src/main/java/com/example/vinhomeproject/config/WebConfig.java
@@ -8,8 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*","http://127.0.0.1:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH");
+        registry.addMapping("*")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
+                .allowedHeaders("*");
     }
 }
