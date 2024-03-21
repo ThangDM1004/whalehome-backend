f187b998f5d42d861b1b5cafbc9c5152fe31a8c5
ThangDM104 <127813818+ThangDM1004@users.noreply.github.com>
2024-03-21 12:31:11 +0700

    socket webconfig update

--- src/main/java/com/example/vinhomeproject/config/WebConfig.java
+++ src/main/java/com/example/vinhomeproject/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH");
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept");
 
     }
 }
